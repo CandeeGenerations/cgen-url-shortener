10837ec8ee537acb9d1122ec4a3a3909374a8bc0
Tyler C <tyler@candeegenerations.com>
2020-09-29 07:29:44 -0400

    feat: initial commit

---
+++ src/modules/app/controllers/app.controller.ts
@@ -0,0 +1,19 @@
+import {Controller, Get, Render} from '@nestjs/common'
+
+import {AppService} from '../services/app.service'
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  @Render('index')
+  root() {
+    return {message: 'Hello world!'}
+  }
+
+  @Get('hello')
+  getHello(): string {
+    return this.appService.getHello()
+  }
+}
